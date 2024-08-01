9e9a80cc56287c42310f08bf5150d7bb0fcfb7da
Thad <thaddeusaraujo@gmail.com>
2024-08-01 14:45:01 -0300

    calc

---
+++ site/calc/js.js
@@ -0,0 +1,18 @@
+function res(event){
+
+    
+
+    var valor = document.getElementById("valor").value;
+    var porc = document.getElementById("porc").value;
+
+    valor = Number(valor);
+    porc = Number(porc);
+  
+    resul = (valor + (valor * (porc / 100));
+
+    document.getElementById("resultado").value = resul;
+ // document.getElementsByTagName("h1")[0].innerHTML = resul;
+
+    
+
+}
